3cd0707784048934f633515f0d8bb87809ceb0a9
Jassi <jkaur@carbonsolutionsgroup.com>
2026-04-08 11:37:38 -0700

    4-1 updates

--- part1-automation.js
+++ part1-automation.js
@@ -10,7 +10,7 @@ const PORTAL_URL = 'https://portal.illinoisabp.com/';
 const ADMIN_LOGIN_URL = 'https://portal2.carbonsolutionsgroup.com/admin/login';
 
 function resolveCredentials() {
-  const abpUsername = process.env.ABP_Email;
+  const abpUsername = process.env.ABP_EMAIL;
   const abpPassword = process.env.ABP_PW;
   const portal2Email = process.env.CSG_ADMIN_EMAIL || process.env.EMAIL;
   const portal2Password = process.env.CSG_ADMIN_PASSWORD || process.env.PASSWORD;
